93d2f3cdb2370d7f636cf46145df0ed0f30557e0
basma92 <basmamostafa1992@gmail.com>
2017-08-04 22:05:59 +0200

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -504,6 +504,7 @@ function updatePositions() {
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    console.log(phase, document.body.scrollTop /1250);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 

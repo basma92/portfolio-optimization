133ac80d833c86847f86f7dfa8f75015d3677595
basma92 <basmamostafa1992@gmail.com>
2017-08-06 03:08:08 +0200

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -449,11 +449,17 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
-    }
+    
+   // used getElementsByClassName instead of using querySelectorAll
+    // for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+     // var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
+      //var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      //document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+     for (var i = 0; i < document.getElementsByClassName("randomPizzaContainer").length; i++) {
+    var dx = determineDx(document.getElementsByClassName("randomPizzaContainer")[i], size);
+      var newwidth = (document.getElementsByClassName("randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      document.getElementsByClassName("randomPizzaContainer")[i].style.width = newwidth;
+  }
   }
 
   changePizzaSizes(size);
